bdf274e5c49ef4d1fb1019ed6b89d562c32bc3ba
Nancy <nancyliao@jubo.health>
2024-03-31 00:04:49 +0800

    feat: Add icon paths

--- lib/constant/icon_path.dart
+++ lib/constant/icon_path.dart
@@ -1,7 +1,9 @@
 class IconPath {
   static const polygon = 'assets/icons/polygon.svg';
+  static const polygonGrey = 'assets/icons/polygon_grey.svg';
   static const medium = 'assets/icons/social_media/medium_logo.svg';
   static const instagram = 'assets/icons/social_media/instagram_logo.svg';
   static const youtube = 'assets/icons/social_media/youtube_logo.svg';
   static const arrow = 'assets/icons/north_east_arrow.svg';
+  static const mobileArrow = 'assets/icons/s_north_east_arrow.svg';
 }
